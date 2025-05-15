231f29c1317f0000fcf52968ace2515cfb1e8427
Emmanuel Mauyano <emmauyano@gmail.com>
2025-05-15 18:23:13 +0200

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Each function renders a page
+def home(request):
+    return render(request, 'main/home.html')
+
+def results(request):
+    return render(request, 'main/results.html')
+
+def admissions(request):
+    return render(request, 'main/admissions.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+
+def about_developer(request):
+    return render(request, 'main/developer_info.html')
