db0af8f7d1afa7e02de7e95f14a4b526468dcea9
Emmanuel Mauyano <emmauyano@gmail.com>
2025-06-13 11:52:19 +0200

    Changed namespace

--- providence_site/settings.py
+++ providence_site/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = 'django-insecure-cs@l2#1s22_44bgzfh$ad@9r=5mec(#ak+mxm95@z_)32hjc4('
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
